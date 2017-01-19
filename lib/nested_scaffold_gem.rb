a1b928ce1aae1e8e1cf8b5fc1fb03b42349a1642
aslanval <valid.aslan@gmx.de>
2017-01-19 21:03:19 +0100

    next level

--- lib/nested_scaffold_gem.rb
+++ lib/nested_scaffold_gem.rb
@@ -4,26 +4,38 @@ class Hola
     translator.hi
   end
 end
+require "nested_scaffold_gem/translator"
+
+#OLD
+#class Nested_scaffold
+#	attr_reader :erste, :zweite
+#	def self.resources(erste, zweite)
+#		@erste = erste
+#		@zweite = zweite
+#	end
+#
+#	def self.erste
+#		@erste
+#	end
+#
+#	def self.zweite
+#		@zweite
+#	end
+#end
 
 class Nested_scaffold
 	attr_reader :erste, :zweite
-	def self.resources(erste, zweite)
+	def initialize(erste, zweite)
 		@erste = erste
 		@zweite = zweite
-		#@erste + " " + @zweite
 	end
 
-	def self.erste
-		@erste
-	end
-
-	def self.zweite
-		@zweite
+	def output
+		puts "Die erste Resource lautet #{@erste} !"
+		puts "Die zweite Resource lautet #{@zweite} !"
+		out_file = File.new("out.txt", "w")
+		out_file.puts("#{@erste} and #{@zweite}")
+		out_file.close
 	end
 end
 
-
-
-require "nested_scaffold_gem/translator"
-
- 
