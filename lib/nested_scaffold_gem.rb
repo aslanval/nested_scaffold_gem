288059f872d102910ea0729e948720059fc7e87d
aslanval <valid.aslan@gmx.de>
2017-01-22 18:52:23 +0100

    Update

--- lib/nested_scaffold_gem.rb
+++ lib/nested_scaffold_gem.rb
@@ -6,36 +6,54 @@ class Hola
 end
 require "nested_scaffold_gem/translator"
 
-#OLD
-#class Nested_scaffold
-#	attr_reader :erste, :zweite
-#	def self.resources(erste, zweite)
-#		@erste = erste
-#		@zweite = zweite
-#	end
-#
-#	def self.erste
-#		@erste
-#	end
-#
-#	def self.zweite
-#		@zweite
-#	end
-#end
-
 class Nested_scaffold
-	attr_reader :erste, :zweite
-	def initialize(erste, zweite)
-		@erste = erste
-		@zweite = zweite
+	attr_reader :first, :second
+	def initialize(first, second)
+		@first = first
+		@second = second
+
+		@first_p = first + "s"
+		@second_p = second + "s"
+
+		@class_erste = first.capitalize
+		@class_zweite = second.capitalize
 	end
 
-	def output
-		puts "Die erste Resource lautet #{@erste} !"
-		puts "Die zweite Resource lautet #{@zweite} !"
-		out_file = File.new("out.txt", "w")
-		out_file.puts("#{@erste} and #{@zweite}")
-		out_file.close
+#	def output
+#		puts "Die erste Resource lautet #{@first} !"
+#		puts "Die zweite Resource lautet #{@second} !"
+#		out_file = File.new("out.txt", "w")
+#		out_file.puts("#{@first} and #{@second}")
+#		out_file.close
+#	end
+
+#	def eins_model
+#		require 'fileutils'
+#
+#		tempfile=File.open("file.tmp", 'w')
+#		f=File.new("movie.rb")
+#		f.each do |line|
+#		  tempfile<<line
+#		  if line.downcase=~/^rofl/
+#		    tempfile << "XXXXX\n"
+#		  end
+#		end
+#		f.close
+#		tempfile.close
+#		FileUtils.mv("file.tmp", "movie.rb")
+#	end
+	def first_model
+		has_many = "	has_many :#{@second_p}, dependent: :destroy\nend"
+		file_names = ['_app/models/movie.rb']
+
+		file_names.each do |file_name|
+  			text = File.read(file_name)
+ 			new_contents = text.gsub(/end/, has_many)
+  			#puts new_contents
+  			File.open(file_name, "w") {|file| file.puts new_contents }
+		end
+
 	end
+
 end
 
